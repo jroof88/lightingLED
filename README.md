59f7ac84048859b8f8b0a19926fb86f5fa16015a
John Roof <jroof@scu.edu>
2016-09-07 21:05:32 -0700

    Updated RGB & README

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Simple project to create LEDs that light up to music using an Arduino and C++!
 - LED Strip
 - Wire
 - C++ Knowledge
+
+This project is never-ending as there are always new combinations to create.
--- RGB.cpp
+++ RGB.cpp
@@ -1,4 +1,4 @@
-// Christmas Setup (Green & Red)
+// Classic RGB setup
 
 #define LEDstripGreen 9
 #define LEDstripRed 10
@@ -14,14 +14,18 @@ void setup() {
 
 void loop() {
   boolean soundstate = digitalRead(7);
-  randNum = random(200);
+  randNum = random(300);
   if (soundstate == HIGH) {
-    if(randNum <= 100){
+    if(randNum < 100){
       analogWrite(LEDstripRed, 0);
       delay(150);
     }
-    else{
+    else if((randNum >= 100) && (randNum >= 200)){
       analogWrite(LEDstripGreen, 0);
+      delay(150);	
+    }
+    else{
+      analogWrite(LEDstripBlue, 0);
       delay(150);
     }
   }
@@ -29,4 +33,5 @@ void loop() {
    analogWrite(LEDstripGreen, 255);
    analogWrite(LEDstripRed, 255);
    analogWrite(LEDstripBlue, 255);
+   
 }
